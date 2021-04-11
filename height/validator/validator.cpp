be502810817834ec0690965ec7e3bb861b2e7e8e
Jonathan Irvin Gunawan <jonathanirvingunawan@gmail.com>
2021-04-11 11:11:24 +0000

    Add validator for height

---
+++ height/validator/validator.cpp
@@ -0,0 +1,50 @@
+#include "testlib.h"
+
+constexpr int kMaxN = 200000;
+constexpr int kMaxQ = 200000;
+constexpr int kMaxH = 1000000000;
+
+int main(int, char *argv[]) {
+  registerValidation();
+
+  const char* subtask_name = argv[1];
+
+  int maxN = kMaxN;
+  int maxQ = kMaxQ;
+  if (strcmp(subtask_name, "quadratic") == 0) {
+    maxN = maxQ = 2000;
+  } else if (strcmp(subtask_name, "sqrt-log") == 0) {
+    maxN = maxQ = 50000;
+  }
+
+  int maxH = strcmp(subtask_name, "small-H") == 0 ? 20 : kMaxH;
+
+  int N = inf.readInt(1, maxN, "N");
+  inf.readSpace();
+  int Q = inf.readInt(1, maxQ, "Q");
+  inf.readEoln();
+  
+  for (int i = 0; i < Q; ++i) {
+    int L = inf.readInt(0,
+                        strcmp(subtask_name, "L-equals-zero") == 0 ? 0 : N - 1,
+                        "L");
+    inf.readSpace();
+    int R = inf.readInt(L,
+                        strcmp(subtask_name, "L-equals-R") == 0 ? L : N - 1,
+                        "R");
+    inf.readSpace();
+    inf.readInt(1, maxH, "H");
+    inf.readEoln();
+
+    if (strcmp(subtask_name, "constant-difference") == 0) {
+      static int query_difference = 0;
+      if (i == 0) {
+        query_difference = R - L;
+      } else {
+        ensuref(R - L == query_difference, "Query difference is not constant");
+      }
+    }
+  }
+  inf.readEof();
+  return 0;
+}
